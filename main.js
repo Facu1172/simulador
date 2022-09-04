0dc483cdd52040ea44de0a8362bb0803e9978431
Facu1172 <Facu172@hotmail.com>
2022-09-04 17:18:32 -0300

    Simulador

---
+++ main.js
@@ -0,0 +1,12 @@
+alert("Este simulador permite calcular el promedio de las notas ingresadas de un alumno")
+
+let nombrealumno = prompt("ingrese el nombre del alumno")
+let cantidadnotas=parseInt(prompt("ingrese la cantidad de notas a ingresar"))
+let acumulador = 0
+let x = 1
+while (x <= cantidadnotas){
+    let notasuma = parseInt(prompt("ingrese las notas de una en una"))
+acumulador = notasuma + acumulador
+x = x + 1
+}
+alert ("El promedio de " + nombrealumno + " es " + acumulador/cantidadnotas)
